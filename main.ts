ae51e77350d52f8a0dce3c88a6c6aaf02dd3ebbf
ansanloms <ansanloms@users.noreply.github.com>
2024-07-27 14:25:10 +0900

    build: upgrade.

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import "std/dotenv/load.ts";
+import "@std/dotenv/load";
 
 import { createBot, Intents, startBot } from "discordeno/mod.ts";
 
@@ -27,43 +27,3 @@ bot.events.interactionCreate = async (b, interaction) => {
 };
 
 await startBot(bot);
-
-//Deno.cron("Stop Minecraft server", "0 4,10,16 * * *", async () => {
-//  try {
-//    const { status } = await minecraftCompute.get();
-//
-//    if (status === "RUNNING") {
-//      await bot.helpers.sendMessage(
-//        Deno.env.get("DISCORD_CHANNEL_ID_MINECRAFT")!,
-//        {
-//          content:
-//            "Minecraft サーバが起動中です。1 分後にシャットダウンします。",
-//        },
-//      );
-//
-//      await setTimeout(async () => {
-//        await minecraftCompute.stop();
-//        const { status } = await minecraftCompute.get();
-//
-//        if (status === "RUNNING") {
-//          await bot.helpers.sendMessage(
-//            Deno.env.get("DISCORD_CHANNEL_ID_MINECRAFT")!,
-//            {
-//              content:
-//                "Minecraft サーバのシャットダウンに失敗している可能性があります。確認してください。",
-//            },
-//          );
-//        } else {
-//          await bot.helpers.sendMessage(
-//            Deno.env.get("DISCORD_CHANNEL_ID_MINECRAFT")!,
-//            {
-//              content: "Minecraft サーバをシャットダウンしました。",
-//            },
-//          );
-//        }
-//      }, 1000 * 60);
-//    }
-//  } catch (err) {
-//    console.error(err);
-//  }
-//});
